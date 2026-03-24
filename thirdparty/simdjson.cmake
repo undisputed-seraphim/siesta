0ec6354d3e1f652ac79b7269b52bcf32fb7a337c
Tan Li Boon <undisputed.seraphim@gmail.com>
2026-03-24 14:52:23 +0800

    WIP

--- thirdparty/simdjson.cmake
+++ thirdparty/simdjson.cmake
@@ -6,8 +6,8 @@ include(FetchContent)
 
 FetchContent_Declare(
     simdjson
-    URL ${CMAKE_SOURCE_DIR}/thirdparty/simdjson-4.0.4.tar.xz
-    URL_HASH MD5=99086bb5353b60d48329c001912739cd
+    URL ${CMAKE_SOURCE_DIR}/thirdparty/simdjson-4.3.1.tar.xz
+    URL_HASH MD5=a887f82618322f5f869bcd1514c4d803
 )
 
 set(SIMDJSON_BUILD_STATIC_LIB CACHE BOOL ON)
