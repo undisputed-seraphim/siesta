6ccc84f8d27a6a4121d53f379f6a60efebc83d68
Tan Li Boon <undisputed.seraphim@gmail.com>
2026-03-24 14:52:23 +0800

    WIP

--- yamltojson.py
+++ yamltojson.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python3
 
+from datetime import datetime, date
 import yaml
 import json
 import sys
+
+def datetime_handler(obj):
+    if isinstance(obj, (datetime, date)):
+        return obj.isoformat()
+    raise TypeError(f"Object of type {type(obj).__name__} is not JSON serializable")
 
 def main():
     with open(sys.argv[1], 'r', encoding="utf-8") as f:
         y = yaml.load(f, Loader=yaml.Loader)
         with open(sys.argv[2], 'w', encoding="utf-8") as fo:
-            fo.write(json.dumps(y, indent='\t', ensure_ascii=False))
+            json.dump(y, fo, indent='\t', default=datetime_handler)
 
 if __name__ == "__main__":
     main()
