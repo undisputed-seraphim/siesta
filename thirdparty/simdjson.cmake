ad4258009aec94757c7b6ba35ea36e0a5cc717dc
Undisputed Seraphim <undisputed.seraphim@gmail.com>
2026-05-03 15:42:06 +0800

    Update simdjson dep

--- thirdparty/simdjson.cmake
+++ thirdparty/simdjson.cmake
@@ -6,8 +6,8 @@ include(FetchContent)
 
 FetchContent_Declare(
     simdjson
-    URL ${CMAKE_SOURCE_DIR}/thirdparty/simdjson-4.3.1.tar.xz
-    URL_HASH MD5=a887f82618322f5f869bcd1514c4d803
+    URL ${CMAKE_SOURCE_DIR}/thirdparty/simdjson-4.6.3.tar.xz
+    URL_HASH MD5=867b4de0a7c7d49ad84e429e9e0418ce
 )
 
 set(SIMDJSON_BUILD_STATIC_LIB CACHE BOOL ON)
